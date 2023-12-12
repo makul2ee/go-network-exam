03c85f724d395029ea060a9df6b73df26346386c
makul2ee <tanaharada11@gmail.com>
2023-12-12 13:51:17 +0700

    update test

--- exam-1/main_test.go
+++ exam-1/main_test.go
@@ -3,19 +3,17 @@ package main
 import "testing"
 
 func TestExampleFunction(t *testing.T) {
-    // TODO: Implement tests for the student's code
-    // Example test (adjust according to the actual assignment)
-    expected := "Hello"
-    if got := HelloFunction(); got != expected {
-        t.Errorf("HelloFunction() = %v, want %v", got, expected)
-    }
+	// TODO: Implement tests for the student's code
+	// Example test (adjust according to the actual assignment)
+	expected := "Hello"
+	if got := HelloFunction(); got != expected {
+		t.Errorf("HelloFunction() = %v, want %v", got, expected)
+	}
 }
-
 
 func TestAdd(t *testing.T) {
     expected := 5
     if got := Add(2, 3); got != expected {
         t.Errorf("Add(2, 3) = %v, want %v", got, expected)
-
     }
 }
